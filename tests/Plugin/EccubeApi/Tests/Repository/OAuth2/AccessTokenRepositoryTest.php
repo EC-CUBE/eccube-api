1b6de8d3752ebceb37a86eb0c6b465dae5d65423
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-04-27 16:47:25 +0900

    AccessTokenRepository のテストケース

--- tests/Plugin/EccubeApi/Tests/Repository/OAuth2/AccessTokenRepositoryTest.php
+++ tests/Plugin/EccubeApi/Tests/Repository/OAuth2/AccessTokenRepositoryTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace Plugin\EccubeApi\Tests\Repository;
+namespace Plugin\EccubeApi\Tests\Repository\OAuth2;
 
 use Eccube\Application;
 use Plugin\EccubeApi\Tests\AbstractEccubeApiTestCase;
@@ -14,13 +14,14 @@ class AccessTokenRepositoryTest extends AbstractEccubeApiTestCase
 {
     protected $Client;
     protected $Customer;
+    protected $token;
 
     public function setUp()
     {
         parent::setUp();
-        $faker = $this->getFaker();
-        $this->Customer = $this->createCustomer();
 
+        $this->Customer = $this->createCustomer();
+        $this->UserInfo = $this->createUserInfo($this->Customer);
         $this->Client = $this->createApiClient(
             $this->Customer,
             'test-client-name',
@@ -28,10 +29,58 @@ class AccessTokenRepositoryTest extends AbstractEccubeApiTestCase
             'test-client-secret',
             'http://example.com/redirect_uri'
         );
+
+        $this->token = 'token-string';
+        $this->expires = time() + 3600;
+        $this->scope = 'openid offline_access';
     }
 
-    public function testGetBaseInfoWithId()
+    public function testSetAccessToken()
     {
-        $AccessTokens = $this->app['eccube.repository.oauth2.access_token']->findAll();
+        $this->app['eccube.repository.oauth2.access_token']->setAccessToken($this->token, 'test-client-id', $this->UserInfo->getId(), $this->expires, $this->scope);
+
+        $AccessToken = $this->app['eccube.repository.oauth2.access_token']->findOneBy(array('token' => $this->token));
+
+        $this->expected = $this->token;
+        $this->actual = $AccessToken->getToken();
+        $this->verify();
+
+        $this->expected = $this->Client;
+        $this->actual = $AccessToken->getClient();
+        $this->verify();
+
+        $this->expected = $this->UserInfo;
+        $this->actual = $AccessToken->getUser();
+        $this->verify();
+
+        $ts = new \DateTime();
+        $this->expected = $ts->setTimestamp($this->expires);
+        $this->actual = $AccessToken->getExpires();
+        $this->verify();
+
+        $this->expected = $this->scope;
+        $this->actual = $AccessToken->getScope();
+        $this->verify();
+    }
+
+    public function testGetAccessToken()
+    {
+        $this->app['eccube.repository.oauth2.access_token']->setAccessToken($this->token, 'test-client-id', $this->UserInfo->getId(), $this->expires, $this->scope);
+
+        $AccessToken = $this->app['eccube.repository.oauth2.access_token']->getAccessToken($this->token);
+        $this->assertTrue(is_array($AccessToken));
+
+        $this->expected = $this->token;
+        $this->actual = $AccessToken['token'];
+        $this->verify();
+
+        $ts = new \DateTime();
+        $this->expected = $this->expires;
+        $this->actual = $AccessToken['expires'];
+        $this->verify();
+
+        $this->expected = $this->scope;
+        $this->actual = $AccessToken['scope'];
+        $this->verify();
     }
 }
