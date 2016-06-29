fbdbb05bff6232319bfe674b2aa7be9bf6bc5436
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-06-29 18:23:42 +0900

    Add copyright

--- tests/Plugin/EccubeApi/Tests/Web/EccubeApiAuthorizationTest.php
+++ tests/Plugin/EccubeApi/Tests/Web/EccubeApiAuthorizationTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the EccubeApi
+ *
+ * Copyright (C) 2016 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Plugin\EccubeApi\Tests\Web;
 
 use Plugin\EccubeApi\Util\EntityUtil;
